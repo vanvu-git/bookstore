dd3bc1a69760b35c29ed1769315161a87b1b724d
Tran Thanh Tung <thanhtung.4work@gmail.com>
2022-11-04 08:14:42 +0700

    change posts to data

--- admin/src/pages/nhaXuatBan/list/TacGiaList.jsx
+++ admin/src/pages/nhaXuatBan/list/TacGiaList.jsx
@@ -13,7 +13,7 @@ export default function TacGiaList() {
   const [data, setData] = useState();
 
   useEffect(() => {
-    axios.get("/tacgia").then(response => {
+    axios.get("/nhaxuatban").then(response => {
       setData(response.data.data);
       setLoading(false);
     })
