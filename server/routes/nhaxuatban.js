b55551d99a19f15519def280b2826f6cb813026e
vanvu-git <vanvu2329@gmail.com>
2022-11-07 19:17:00 +0700

    fix sach nxb

--- server/routes/nhaxuatban.js
+++ server/routes/nhaxuatban.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const verifyToken = require('../middleware/auth');
+const {verifyToken} = require('../middleware/auth');
 const nhaxuatbanController = require('../controllers/nhaxuatban');
 
 router.post('/', nhaxuatbanController.create);
--- server/routes/sach.js
+++ server/routes/sach.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const verifyToken = require('../middleware/auth');
+const {verifyToken} = require('../middleware/auth');
 const sachController = require('../controllers/sach');
 
 router.post('/', sachController.create);
